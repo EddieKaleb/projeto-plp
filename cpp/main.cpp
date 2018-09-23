78b9a975322cd1e41f1736e1b78f1be0f61f4836
ViniFarias <marcus.barbosa@gmail.com>
2018-09-23 18:15:28 -0300

    Adição do arquivo main.cpp, e inicio do desenvolvimento do menu inicial

---
+++ cpp/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void showMenu();
+void clearScreen();
+int getOption();
+void selectMenuOption(int option);
+void showGameModesMenu();
+void selectGameModeOption(int option);
+void quit();
+
+void clearScreen() {
+    system("clear");
+}
+
+void showMenu() {
+    clearScreen();
+
+    cout << endl;
+    cout << "---------------------------------     MENU     ---------------------------------";
+    cout << endl << endl;
+
+    cout << "           1  -  Modos de jogo" << endl;
+    cout << "           2  -  Regras" << endl;
+    cout << "           3  -  Ranking" << endl;
+    cout << "           4  -  Sair" << endl;
+
+    selectMenuOption(getOption());
+}
+
+int getOption() {
+    int option;
+
+    cout << endl << "           Digite o número da opção desejada: ";
+    cin >> option;
+
+    return option;
+}
+
+void selectMenuOption(int option) {
+    switch(option) {
+        case 1:
+            showGameModesMenu();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            quit();
+            break;
+        default:
+            break;
+    }
+}
+
+void showGameModesMenu() {
+     clearScreen();
+
+     cout << endl;
+     cout << "-----------------------------     MODOS DE JOGO     -----------------------------";
+     cout << endl << endl;
+
+     cout << "          1  -  Partida Casual" << endl;
+     cout << "          2  -  Modo Aprendizado" << endl;
+     cout << "          3  -  Voltar" << endl;
+
+     selectGameModeOption(getOption());
+}
+
+void selectGameModeOption(int option) {
+    switch(option) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
+}
+
+void quit() {
+    clearScreen();
+
+    cout << endl << endl;
+
+    cout << "                                    Até mais!";
+    cout << endl << endl << endl;
+
+    cout << "             Paradigmas de Linguagem de Programação - 2018.2 - UFCG";
+    cout << endl << endl << endl;
+
+    cout << "                                DESENVOLVIDO POR:";
+    cout << endl << endl;
+
+    cout << "                              Arthur de Lima Ferrão" << endl;
+    cout << "                           Eddie Kaleb Lopes Fernandes" << endl;
+    cout << "                             Gabriel de Sousa Barros" << endl;
+    cout << "                        Marcus Vinícius de Farias Barbosa" << endl;
+    cout << "                              Rayla Medeiros Araújo" << endl;
+    cout << endl << endl;
+
+    exit(1);
+}
+
+int main() {
+
+    while(true) {
+        showMenu();
+    }
+
+    return 0;
+}
