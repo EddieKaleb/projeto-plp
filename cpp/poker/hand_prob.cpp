b698bd4978a11dd6225c11a1f9258566007b8e93
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-09-24 23:23:18 -0300

    - Finalizado a extração das probabilidades

--- cpp/poker/hand_prob.cpp
+++ cpp/poker/hand_prob.cpp
@@ -1,21 +1,76 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    ifstream ip("../data/hands.csv");
+string get_probability(string probs, int num_players);
+float hand_probability(string target_hand, int num_players);
 
-    if (!ip.is_open())
-        cout << "ERROR: file open" << '\n';
+const string CSV_PATH = "../data/hands.csv";
+const string DEFAULT = "0.0";
+const int MIN_PLAYERS = 2, MAX_PLAYERS = 10;
 
-    string cards;
-    string players;
+/*
+    Returns the win probability given a poker hand and number of
+    players. 
+*/
+float hand_probability(string target_hand, int num_players) {
+    ifstream ip(CSV_PATH);
 
-    while (ip.good()) {
-        getline(ip, cards,',');
-        getline(ip, players,'\n');
-        cout << "Card: " << cards << " " << players << endl;
+    string result_prob = DEFAULT;
+    string hand, probabilities;
+
+    if (!ip.is_open()) {
+        cout << "ERROR: File path not found." << '\n';
+    } else {
+        do {
+            getline(ip,hand,',');
+            getline(ip,probabilities,'\n');
+        } while (ip.good() && hand != target_hand);
     }
-    return 0;
+
+    if (hand == target_hand) {
+        result_prob = get_probability(probabilities, num_players);
+    } else {
+        cout << "ERROR: Invalid poker hand." << '\n';
+    }
+
+    return atof(result_prob.c_str());
+}
+
+/*
+    Splits probabilities' line extracted from csv file and
+    returns the probability that corresponds the number 
+    of players. 
+*/
+string get_probability(string probs, int num_players) {
+    string target_probability;
+
+    if (num_players >= MIN_PLAYERS && num_players <= MAX_PLAYERS) {
+        int count = 0;
+
+        while (count <= (num_players - 2)) {
+            target_probability = probs.substr(0, probs.find(','));
+            probs.erase(0, probs.find(',') + 1);
+            count++;
+        }
+    } else {
+        cout << "ERROR: Invalid number of players." << '\n';
+    }
+
+    return target_probability;
+}
+
+/*
+    Temporary test method
+*/
+int main() {
+    cout << hand_probability("AA", 2) << endl;
+    cout << hand_probability("AA", 3) << endl;
+    cout << hand_probability("22", 3) << endl;
+    cout << hand_probability("KK", 7) << endl;
+    
+    cout << hand_probability("WWWW", 3) << endl;
+    cout << hand_probability("KK", 12) << endl;
 }
