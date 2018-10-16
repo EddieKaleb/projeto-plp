d26efdf0f6de9f0a9346b283ff953b4c3b2a46b5
ViniFarias <vinifarias.vf@gmail.com>
2018-10-16 17:53:04 -0300

    Desenvolvimento do menu inicial

--- haskell/poker/main.hs
+++ haskell/poker/main.hs
@@ -1,3 +1,11 @@
+import System.IO
+import Data.Char
+import Data.List
+import Data.Ord
+import Data.Time.Clock.POSIX
+import qualified System.Process
+import Control.Monad
+
 sleep3s :: IO()
 sleep3s = do
     _ <- System.Process.system "sleep 3s"
@@ -8,8 +16,15 @@ clearScreen = do
     _ <- System.Process.system "clear"
     return ()
 
+pause :: IO ()
+pause = do
+    x <- getChar
+    putStrLn ""
+
+
 showTitle :: IO()
 showTitle = do
+    putStrLn "\n\n"
     putStrLn "               $$$$$$$\134   $$$$$$\134  $$\134   $$\134 $$$$$$$$\134 $$$$$$$\134"
     putStrLn "               $$  __$$\134 $$  __$$\134 $$ | $$  |$$  _____|$$  __$$\134"
     putStrLn "               $$ |  $$ |$$ /  $$ |$$ |$$  / $$ |      $$ |  $$ |"
@@ -17,8 +32,7 @@ showTitle = do
     putStrLn "               $$  ____/ $$ |  $$ |$$  $$<   $$  __|   $$  __$$< "
     putStrLn "               $$ |      $$ |  $$ |$$ |\134$$\134  $$ |      $$ |  $$ |"
     putStrLn "               $$ |       $$$$$$  |$$ | \134$$\134 $$$$$$$$\134 $$ |  $$ |"
-    putStrLn "               \134__|       \134______/ \134__|  \134__|\134________|\134__|  \134__|"
-    putStrLn " ";
+    putStrLn "               \134__|       \134______/ \134__|  \134__|\134________|\134__|  \134__|\n"
     putStrLn "                                  Carregando..."
 
     sleep3s
@@ -28,7 +42,117 @@ showMenu = do
     clearScreen
 
     putStrLn " "
-    putStrLn "---------------------------------     MENU     ---------------------------------"
+    putStrLn "---------------------------------     MENU     ---------------------------------\n\n"
     putStrLn "           1  -  Modos de jogo"
     putStrLn "           2  -  Regras"
     putStrLn "           3  -  Sair"
+
+    option <- getOption
+    selectMenuOption option
+    clearScreen
+    when (not $ option == 3) $ do clearScreen; showMenu
+
+showGameModesMenu :: IO()
+showGameModesMenu = do
+    clearScreen
+
+    putStrLn "\n-----------------------------     MODOS DE JOGO     -----------------------------\n\n";
+
+    putStrLn "          1  -  Partida Casual"
+    putStrLn "          2  -  Modo Aprendizado"
+    putStrLn "          3  -  Voltar"
+
+    option <- getOption
+    selectGameModeOption option
+
+showRules :: IO()
+showRules = do
+    clearScreen
+
+    putStrLn "\n----------------------------------     REGRAS     --------------------------------\n\n"
+    putStrLn "------------------------------  LIMIT TEXAS HOLD'EM  -----------------------------\n"
+    putStrLn "As apostas no Limit são valores estruturados e pré-determinados.\n"
+    putStrLn "No pré-flop e no flop, todas as apostas e raises são do mesmo valor do big blind.\n"
+    putStrLn "No turn e no river, o tamanho de todas as apostas e raises é dobrado.\n\n"
+    putStrLn "-----------------------------------    MECANICA    -------------------------------\n"
+    putStrLn "O botão de Dealer gira ao longo da partida de forma a alternar as posições na mesa.\n"
+    putStrLn "O botão de Small Blind determina quem inicia com a menor aposta obrigatoria.\n"
+    putStrLn "O botão de Big Blind determina quem inicia com a maior aposta obrigatoria.\n"
+    putStrLn "O valor do Small Blind é 2 e do Big Blind é 4.\n"
+    putStrLn "O jogador sempre sera o player1.\n\n"
+    putStrLn "------------------------------------    AÇÕES     --------------------------------\n"
+    putStrLn "                               CALL: cobrir uma aposta.\n"
+    putStrLn "                               CHECK: passar a vez.\n"
+    putStrLn "                               FOLD: desistir.\n\n"
+    putStrLn "-----------------------------------     TURNOS     --------------------------------\n"
+    putStrLn "PRE-FLOP: O Dealer é escolhido através de sorteio e cada jogador recebe 2 cartas.\n"
+    putStrLn "FLOP: O Crupier coloca 3 cartas na mesa.\n"
+    putStrLn "TURN: O Crupier coloca 1 carta na mesa\n"
+    putStrLn "RIVER: O Crupier coloca 1 carta na mesa\n\n\n"
+    putStrLn "                         [ Pressione ENTER para voltar ]\n"
+
+    _ <- getLine
+    return ()
+
+
+getOption :: IO Int
+getOption = do
+    putStrLn "\n\n                    Digite o número da opção desejada: "
+    option <- getLine
+    return $ read option
+
+selectMenuOption :: Int -> IO()
+selectMenuOption 1 = showGameModesMenu
+selectMenuOption 2 = showRules
+selectMenuOption 3 = quit
+selectMenuOption n = showInvalidOptionMessage
+
+selectGameModeOption :: Int -> IO()
+selectGameModeOption 1 = casualMatch
+selectGameModeOption 2 = manualMatch
+selectGameModeOption 3 = showMenu
+selectGameModeOption n = showInvalidOptionMessage
+
+casualMatch :: IO()
+casualMatch = do
+    putStrLn "casualMatch"
+
+manualMatch :: IO()
+manualMatch = do
+    putStrLn "manualMatch"
+
+showInvalidOptionMessage :: IO()
+showInvalidOptionMessage = do
+    putStrLn "           Opção inválida... Pressione ENTER para tentar novamente!\n"
+    
+    _ <- getLine
+    return ()
+
+
+quit :: IO()
+quit = do
+    clearScreen
+
+    putStrLn "\n\n"
+
+    putStrLn "                                    Até mais!"
+    putStrLn "\n\n\n"
+
+    putStrLn "             Paradigmas de Linguagem de Programação - 2018.2 - UFCG"
+    putStrLn "\n\n\n"
+
+    putStrLn "                                DESENVOLVIDO POR:"
+    putStrLn "\n\n"
+
+    putStrLn "                              Arthur de Lima Ferrão"
+    putStrLn "                           Eddie Kaleb Lopes Fernandes"
+    putStrLn "                             Gabriel de Sousa Barros"
+    putStrLn "                        Marcus Vinícius de Farias Barbosa"
+    putStrLn "                              Rayla Medeiros Araújo"
+    sleep3s
+
+
+main :: IO()
+main = do
+    showTitle
+    showMenu
