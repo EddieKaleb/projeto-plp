d42146b834a9dec6c6473b1675f32b0be1bd8cbc
ViniFarias <vinifarias.vf@gmail.com>
2018-10-20 17:29:45 -0300

    Desenvolvimento da lógica das partidas

--- haskell/poker/table.hs
+++ haskell/poker/table.hs
@@ -2,6 +2,76 @@ import System.IO
 import Data.Char
 import qualified System.Process
 
+
+-- Representação de uma carta
+data Card = Card {
+    naipe :: String
+    value :: String
+} deriving (Show)
+
+-- Representação de um jogador
+data Player = Player {
+    hand :: [Card],
+    chips :: Int
+} deriving (Show)
+
+-- Representação de um deck
+data Deck = Deck {
+    cards :: [Card]
+} deriving (Show)
+
+{-
+    Inicia o jogo
+-}
+startGame :: IO()
+startGame = do
+    -- playersTable = setPlayers (Configura os jogadores da partida, com um valor default de chips)
+    -- dealer_position = setInitialDealerPosition (Configura a posição inicial do dealer de forma aleatória)
+
+    runMatch
+
+
+{-
+    Roda uma partida.
+-}
+runMatch :: IO()
+    -- deck = buildDeck
+    -- cardsTable = setCardsTable
+
+    runTurn 0
+
+
+{-
+    Roda os quatro turnos de uma partida.
+    @param Int Identificador do turno.
+-}
+runTurn :: Int -> IO()
+runTurn turnId
+    | turnId < 4 = do
+                    selectTurn turnId
+                    runTurn (turnId + 1)
+    | otherwise = return
+
+
+{-
+    Executa um método de turno específico com base no identificador do turno.
+    @param Int Identificador do turno.
+-}
+selectTurn :: Int -> IO()
+selectTurn 0 = preFlopRound
+selectTurn 1 = flopRound
+selectTurn 2 = turnRound
+selectTurn 3 = riverRound
+
+
+{-
+    Retorna a posição do próximo jogador com base na posição do jogador atual.
+    @param Int Posição do jogador atual.
+    @param Int Quantidade de jogadores.
+-}
+nextPlayerPosition :: Int -> Int -> Int
+nextPlayerPosition position qtdPlayers = (position + 1) 'mod' qtdPlayers
+
 clearScreen :: IO()
 clearScreen = do
     _ <- System.Process.system "clear"
