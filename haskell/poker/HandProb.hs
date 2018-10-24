fa0e9b094225be998844f6108751cd6a109af05c
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-10-24 20:36:01 -0300

    extracao funcional (gloria a deux)

--- haskell/poker/HandProb.hs
+++ haskell/poker/HandProb.hs
@@ -7,20 +7,22 @@ data Prob = Prob {
     probs :: [String]
 } deriving (Show);
 
+{-
 extract :: String -> Int -> [String] -> String
 extract _ _ [] = ""
 extract hand numPlayers (x:xs) 
         | (split(x)!!0 == hand) = split(x)!!(numPlayers-1)                               
         | otherwise = extract hand numPlayers xs
-        
+-}
+
 split :: String -> [String]
 split [] = [""]
 split (c:cs) | c == ','  = "" : rest
              | otherwise = (c : head rest) : tail rest
                 where rest = split cs
                
-extract_prob :: Prob -> Int -> String
-extract_prob handProbs numPlayers = extract (hand handProbs) numPlayers (probs handProbs) 
+extract_prob :: [String] -> Int -> String
+extract_prob handsProb numPlayers = handsProb!!(numPlayers-2)
 
 setProbabilities :: IO [Prob]
 setProbabilities = do
@@ -35,15 +37,12 @@ setProbabilities = do
             where createProb (hand:probs) = Prob hand probs
 
 find_prob :: String -> [Prob] -> [String]
-find_prob _ [] = []
-find_prob playerHand (x:xs) | (playerHand == (hand x)) = (probs x)
-                              | otherwise = find_prob playerHand xs
-
-main :: IO()
-main = do
-    let hand = "22"
-    all_probs <- setProbabilities
-    putStrLn all_probs
-    --let prob = find_prob hand all_probs 
-    --show (extract_prob prob 2)
-    --putStrLn ""
+find_prob _ [] = [""]
+find_prob playerHand (head:tail) | (playerHand == (hand head)) = (probs head)
+                              | otherwise = find_prob playerHand tail
+main :: String -> Int -> IO()
+main hand numPlayers = do
+    probs <- setProbabilities
+    let res = find_prob hand probs
+    let prob = extract_prob res numPlayers
+    putStrLn (show prob)
