ede5ba398b1d2c86cfea2ab5642ca57fe8584698
Eddie Kaleb <eddie.fernandes@virtus.ufcg.edu.br>
2018-10-24 11:16:35 -0300

    atualizacao na extracao

--- haskell/poker/hand_prob.hs
+++ haskell/poker/hand_prob.hs
@@ -1,26 +1,35 @@
-module HandProb (extract_prob) where
+module HandProb where
 
-import AllHands
-import System.IO 
+import System.IO
+
+data Prob = Prob {
+    hand  :: String,
+    probs :: [String]
+} deriving (Show);
 
 extract :: String -> Int -> [String] -> String
 extract _ _ [] = ""
 extract hand numPlayers (x:xs) 
         | (split(x)!!0 == hand) = split(x)!!(numPlayers-1)                               
         | otherwise = extract hand numPlayers xs
-
+        
 split :: String -> [String]
 split [] = [""]
 split (c:cs) | c == ','  = "" : rest
              | otherwise = (c : head rest) : tail rest
-               where rest = split cs
+                where rest = split cs
+               
+--extract_prob :: String -> Int -> String
+--extract_prob hand numPlayers = extract hand numPlayers getHands
 
-extract_prob :: String -> Int -> String
-extract_prob hand numPlayers = extract hand numPlayers getHands
-
-read_file :: IO() 
-read_file = do
-    hands_file <- openFile "../data/hands.csv" ReadMode
-    probs <- hGetContents hands_file
-    -- extrair prob -- 
-    hClose hands_file
+setProbabilities :: IO [Prob]
+setProbabilities = do
+    probs <- readProbs
+    return $ createList probs
+    where readProbs = do
+              probs <- openFile "../data/hands.csv" ReadMode
+              contents <- hGetContents probs
+              return $ lines contents
+          createList [] = []
+          createList (head:tail) = [createProb $ split head] ++ createList tail
+            where createProb (hand:probs) = Prob hand probs
