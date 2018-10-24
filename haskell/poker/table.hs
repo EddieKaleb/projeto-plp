7b41143fa18d0ef79f9c59c66c5e792a0cf2f238
ViniFarias <vinifarias.vf@gmail.com>
2018-10-24 15:14:06 -0300

    Adição do import de Model na table.hs

--- haskell/poker/table.hs
+++ haskell/poker/table.hs
@@ -1,48 +1,8 @@
 import System.IO
 import Data.Char
 import qualified System.Process
+import Model
 
-{-
-    Representação de uma carta.
--}
-data Card = Card {
-    naipe :: String,
-    value :: String
-} deriving (Show)
-
-
-{-
-    Representação de um jogador.
--}
-data Player = Player {
-    hand :: [Card],
-    chips :: Int,
-    active :: Bool
-} deriving (Show)
-
-
-{-
-    Representação de um deck.
--}
-data Deck = Deck {
-    cards :: [Card]
-} deriving (Show)
-
-
-{-
-    Representação de um estado do jogo.
--}
-data GameStatus = GameStatus {
-    cardsTable :: [Card],
-    playersTable :: [Player],
-    dealerPosition :: Int,
-    lastBet :: Int,
-    minimumBet :: Int,
-    activePlayers :: Int,
-    currentRound :: Int,
-    userPosition :: Int,
-    pot :: Int
-} deriving (Show)
 
 
 {-
@@ -100,7 +60,7 @@ preFlopRound gameStatus = do
 
     let isValidA = callAction smallPos
     let isValidB = callAction bigPos
-    let newGameStatus = preFlopActions (nextPlayerPosition bigPos) bigPos gameStatus
+    newGameStatus <- preFlopActions (nextPlayerPosition bigPos) bigPos gameStatus
     flopRound newGameStatus
 
 flopRound :: GameStatus -> IO GameStatus
@@ -226,8 +186,7 @@ sleep seg = do
 setNextDealerPosition :: GameStatus -> GameStatus
 setNextDealerPosition gs = do
     let newDealer = nextPlayerPosition (dealerPosition gs)
-    GameStatus (cardsTable gs) (playersTable gs) (newDealer) (lastBet gs) (minimumBet gs) (activePlayers gs) (currentRound gs) (userPosition gs) (pot gs)
-
+    setDealerPosition newDealer gs
 	
 invalidAction :: IO()
 invalidAction = putStrLn("Ação inválida")
@@ -287,65 +246,7 @@ exitAction = do
 	-- Faltou exit
 
 
----------- MÉTODOS AUXILIARES DE MANIPULAÇÃO DA GAMESTATUS  ----------
 
-setPot :: Int -> GameStatus -> GameStatus
-setPot value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) (playersTable gameStatus) (dealerPosition gameStatus)
-         (lastBet gameStatus) (minimumBet gameStatus) (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) value
-
-    gm
-
-
-setDealerPosition :: Int -> GameStatus -> GameStatus
-setDealerPosition value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) (playersTable gameStatus) value
-         (lastBet gameStatus) (minimumBet gameStatus) (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
-setLastBet :: Int -> GameStatus -> GameStatus
-setLastBet value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) (playersTable gameStatus) (dealerPosition gameStatus)
-         value (minimumBet gameStatus) (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
-setCurrentRound :: Int -> GameStatus -> GameStatus
-setCurrentRound value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) (playersTable gameStatus) (dealerPosition gameStatus)
-         (lastBet gameStatus) (minimumBet gameStatus) (activePlayers gameStatus) value (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
-setMinimumBet :: Int -> GameStatus -> GameStatus
-setMinimumBet value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) (playersTable gameStatus) (dealerPosition gameStatus)
-         (lastBet gameStatus) value (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
-setCardsTable :: [Card] -> GameStatus -> GameStatus
-setCardsTable value gameStatus = do
-    let gm = GameStatus value (playersTable gameStatus) (dealerPosition gameStatus)
-         (lastBet gameStatus) (minimumBet gameStatus) (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
-setPlayersTable :: [Player] -> GameStatus -> GameStatus
-setPlayersTable value gameStatus = do
-    let gm = GameStatus (cardsTable gameStatus) value (dealerPosition gameStatus)
-         (lastBet gameStatus) (minimumBet gameStatus) (activePlayers gameStatus) (currentRound gameStatus) (userPosition gameStatus) (pot gameStatus)
-
-    gm
-
-
----------- FIM DOS MÉTODOS AUXILIARES DE MANIPULAÇÃO DA GAMESTATUS
 
 main :: IO ()
 main = do
