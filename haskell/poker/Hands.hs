ea3ad1d8ce8e6a1d50b5bb6c0734269a83e09957
Arthur Ferrão <arthur.ferrao@embedded.ufcg.edu.br>
2018-10-26 10:32:18 -0300

    Ajuste no metodo verifyHand

--- haskell/poker/Hands.hs
+++ haskell/poker/Hands.hs
@@ -1,37 +1,43 @@
-
+module Hands (verifyHand) where
 import Model
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 verifyHand :: [Card] -> [Card] -> Int -> String
-verifyHand hand table quant = verifyFlush (hand ++ (take quant table)) 
+verifyHand hand table quant | verifyFour (hand ++ (take quant table)) = "IS_FOUR"
+                            | verifyFlush (hand ++ (take quant table)) = "IS_FLUSH"
+                            | verifyThree (hand ++ (take quant table)) = "IS_THREE"
+                            | verifyTwoPair (hand ++ (take quant table)) = "IS_TWO_PAIR"
+                            | verifyOnePair (hand ++ (take quant table)) = "IS_ONE_PAIR"
 
 
-verifyPair :: [Card] -> String
-verifyPair [] = "null"
-verifyPair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 = "IS_ONE_PAIR"
-                  | otherwise = verifyPair cs
+verifyOnePair :: [Card] -> Bool
+verifyOnePair [] = False
+verifyOnePair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 = True
+                     | otherwise = verifyOnePair cs
 
 
-verifyThree :: [Card] -> String
-verifyThree [] = "null"
-verifyThree (c:cs) | length (filter ((value c)==) [value x| x <- cs]) == 2 = "IS_THREE"
-               | otherwise = verifyThree cs
+verifyThree :: [Card] -> Bool
+verifyThree [] = False
+verifyThree (c:cs) | length (filter ((value c)==) [value x| x <- cs]) == 2 = True
+                   | otherwise = verifyThree cs
 
 
-verifyFour :: [Card] -> String
-verifyFour [] = "null"
-verifyFour (c:cs) | length (filter ((value c)==) [value x| x <- cs]) == 3 = "IS_FOUR"
+verifyFour :: [Card] -> Bool
+verifyFour [] = False
+verifyFour (c:cs) | length (filter ((value c)==) [value x| x <- cs]) == 3 = True
                   | otherwise = verifyFour cs
 
 
-verifyTwoPair :: [Card] -> String
-verifyTwoPair [] = "null"
+verifyTwoPair :: [Card] -> Bool
+verifyTwoPair [] = False
 verifyTwoPair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 = auxVerifyTwoPair cs
                      | otherwise = verifyTwoPair cs
 
 
-auxVerifyTwoPair :: [Card] -> String
-auxVerifyTwoPair [] = "null"
-auxVerifyTwoPair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 = "IS_TWO_PAIR"
+auxVerifyTwoPair :: [Card] -> Bool
+auxVerifyTwoPair [] = False
+auxVerifyTwoPair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 = True
                         | otherwise = auxVerifyTwoPair cs
 
 
@@ -42,11 +48,12 @@ auxVerifyTwoPair (c:cs) | length (filter  ((value c)==)[value x| x <- cs]) == 1 
 
 
 -- auxVerifyFullHouse :: [String] -> String
--- auxVerifyFullHouse [] = "null"
+-- auxVerifyFullHouse [] = False
 -- auxVerifyFullHouse (c:cs) | length (filter  (c==)cs) == 2 = "IS_FULL_HOUSE"
 --                         | otherwise = auxVerifyFullHouse cs
 
-verifyFlush :: [Card] -> String
-verifyFlush [] = "null"
-verifyFlush (c:cs) | length (filter  ((naipe c)==)[naipe x| x <- (c:cs)]) >= 5 = "IS_FLUSH"
+verifyFlush :: [Card] -> Bool
+verifyFlush [] = False
+verifyFlush (c:cs) | length (filter  ((naipe c)==)[naipe x| x <- (c:cs)]) >= 5 = True
                    | otherwise = verifyFlush cs
+
