be10a72f4114888d959b78404dbdb97bb1cd89c0
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-11-18 17:13:52 -0300

    calculo de prob. da mao melhorar

--- prolog/table.pl
+++ prolog/table.pl
@@ -6,3 +6,14 @@ start_game :-
 start_game_manual :-
     sleep(3),
     writeln("Manual Match").    
+
+get_improve_prob("IS_ONE_PAIR", Iprob) :- improve_prob(5, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_TWO_PAIR", Iprob) :- improve_prob(4, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_THREE", Iprob) :- improve_prob(8, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_STRAIGHT", Iprob) :- improve_prob(1, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_FLUSH", Iprob) :-  improve_prob(2, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_FULL_HOUSE", Iprob) :-  improve_prob(1, Prob), Iprob is Prob / 100.
+get_improve_prob("IS_HIGH_CARD", Iprob) :- improve_prob(15, Prob), Iprob is Prob / 100.
+get_improve_prob(_, Iprob) :- improve_prob(0, Prob), Iprob is Prob / 100.
+
+improve_prob(Outs, Prob) :- Prob is (Outs * 4) - (Outs - 8).
