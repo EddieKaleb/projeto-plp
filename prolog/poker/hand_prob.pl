addf5ffcf7d6b9902a50130293a039d5af72022f
Eddie Kaleb <kaleb.eddie964@gmail.com>
2018-11-26 23:20:16 -0300

    Correcao na extracao de prob.

--- prolog/poker/hand_prob.pl
+++ prolog/poker/hand_prob.pl
@@ -2,7 +2,6 @@
 :- module(hand_prob, [get_hand_prob/4]).
 :- use_module(library(apply)).
 :- use_module(library(csv)).
-:- set_prolog_flag(double_quotes, chars).
 
 get_hand_prob(Card1, Card2, Numplayers, Prob) :-
   get_rows_data("../data/hands.csv", L), 
@@ -12,8 +11,7 @@ get_hand_prob(Card1, Card2, Numplayers, Prob) :-
 
 get_hand(_, _, [], 0). 
 get_hand(Hand, Numplayers, [[H|Ta]|Tb], Handprob) :-
-  H = Hand,
-  nth1(Numplayers, Ta, Handprob); 
+  atom_string(H,HStr),HStr = Hand -> nth1(Numplayers, Ta, Handprob); 
   get_hand(Hand, Numplayers, Tb, Handprob).
 
 get_rows_data(File, Lists):-
@@ -27,13 +25,6 @@ row_to_list(Row, List):-
   Row =.. [row|List].
 
 format_hand([N1,V1],[N2,V2],Formated):-
-  V1 =\= V2, N1 =:= N2,
-    append(V1,V2,Values),
-    append(Values,"s",Aux),
-    atom_string(Aux,Formated);
-  V1 =\= V2, N1 =\= N2,  
-    append(V1,V2,Values),
-    append(Values,"o",Aux),
-    atom_string(Aux,Formated);
-  V1 =:= V2,
-    append(V1,V2,Values),atom_string(Values,Formated).
+  V1 =\= V2,N1 =:= N2 -> string_concat(V1,V2,Values),string_concat(Values,'s',Formated);
+  V1 =\= V2, N1 =\= N2 -> string_concat(V1,V2,Values), string_concat(Values,'o',Formated);
+  string_concat(V1,V2,Formated).
