b93c5018f346bf3c9038153e4cfb20cd97af7f00
arthurferrao <arthurdeferrao@gmail.com>
2018-11-28 22:16:28 -0300

    Gerar deck e pegar cartas por index

--- prolog/poker/deck.pl
+++ prolog/poker/deck.pl
@@ -1,9 +1,38 @@
+:- module(
+    deck, 
+    [buildDeck/0,
+    getCardByIndex/2
+    ]).
+
+:- dynamic(deck/1).
+deck([]).
+
+setDeck(Deck):-  
+    retract(deck(_)),
+    asserta(deck(Deck)).
+
+buildDeck():- 
+    build_deck(["A","K","Q","J","T","9","8","7","6","5","4","3","2"], ["E","C","P","O"], Deck),
+    setDeck(Deck).
+
+build_deck(Values,[H],Deck):- 
+    create(H,Values,Deck).
+build_deck(Values,[H|T],Deck):- 
+    create(H,Values,Part), 
+    build_deck(Values,T,D),
+    append(Part, D, X), 
+    Deck = X.
+
+create(Nipe,[H],Part):- 
+    Part = [[Nipe,H]].
+create(Nipe,[H|T],Part):- 
+    create(Nipe,T,P), 
+    append(P, [[Nipe,H]], X), 
+    Part = X.
 
 
-buildDeck(Deck):- build_deck(["A","K","Q","J","T","9","8","7","6","5","4","3","2"], ["E","C","P","O"], Deck).
+getCardByIndex(Index,Card):-
+    deck(X), 
+    nth0(Index,X,Value), 
+    Card = Value.
 
-build_deck(Values,[H],Deck):- create(H,Values,Deck).
-build_deck(Values,[H|T],Deck):- (create(H,Values,Part), build_deck(Values,T,D),append(Part, D, X)) -> (Deck = X).
-
-create(Nipe,[H],Part):- Part = [[Nipe,H]].
-create(Nipe,[H|T],Part):- create(Nipe,T,P), append(P, [[Nipe,H]], X), Part = X.
