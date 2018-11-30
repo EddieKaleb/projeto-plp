92a8cf702a45c4e3dcc86ad237a620298da262a8
Arthur Ferrão <arthurdeferrao@gmail.com>
2018-11-30 11:06:37 -0300

    Adiciona verifyStraight e verifyFlush

--- prolog/poker/hands.pl
+++ prolog/poker/hands.pl
@@ -3,12 +3,12 @@
 verifyHand(Cards,Hand):-quick_sort(Cards,Sorted), verify_hand(Sorted,Hand).
 verify_hand(Cards,Hand):- % verifyFour(Cards,Hand), Hand == "IS_FOUR";
                           % verifyFullHouse(Cards,Hand), Hand == "IS_FULL_HOUSE";
-                          % verifyFlush(Cards,Hand), Hand == "IS_FLUSH";
-                          % verifyStraight(Cards,Hand), Hand == "IS_STRAIGHT";
+                          verifyFlush(Cards) -> (Hand = "IS_FLUSH");
+                          verifyStraight(Cards) -> (Hand = "IS_STRAIGHT");
                           verifyThree(Cards) -> (Hand = "IS_THREE");
                           verifyTwoPair(Cards) -> (Hand = "IS_TWO_PAIR");
                           verifyOnePair(Cards) -> (Hand = "IS_ONE_PAIR");
-                          Hand = "null".
+                          Hand = "IS_HIGH_CARD".
 
 verifyOnePair([_]):- false.
 verifyOnePair([X,Y|T]):- nth0(1,X,ValueX), nth0(1,Y,ValueY), ValueX==ValueY; 
@@ -22,11 +22,22 @@ verifyThree([_,_]):- false.
 verifyThree([X,Y,Z|T]):- nth0(1,X,ValueX), nth0(1,Y,ValueY), nth0(1,Z,ValueZ), ValueX==ValueY, ValueZ==ValueY; 
                          verifyThree([Y,Z|T]).
  
+verifyStraight(Cards):- verify_straight(Cards,1).
+verify_straight(_, 5).
+verify_straight([X,Y|T],Cont):-
+    map_cards(X,X1),
+    map_cards(Y,Y1), 
+    Y2 is Y1 + 1,
+    X1 == Y2 ,
+    Cont1 is Cont + 1, 
+    verify_straight([Y|T],Cont1); 
+    Cont = 1,
+    verify_straight([Y|T],Cont).
 
-verify_straight([_], Cont):- Cont >= 5.
-verify_straight([X,Y|T],Cont):- nth0(1,X,ValueX), nth0(1,Y,ValueY), ValueX==ValueY+1 -> (Cont = Cont + 1, verify_straight([Y|T],Cont)); 
-                                Cont = 0, verify_straight([Y|T],Cont).
+verifyFlush(Cards):-verify_flush(Cards,"O", 5);verify_flush(Cards,"P", 5);verify_flush(Cards,"E", 5);verify_flush(Cards,"C", 5).
 
+verify_flush([],_, Cont):- Cont=<0.
+verify_flush([H|T],Naipe, Cont):- nth0(0,H,Cn), Cn == Naipe, Cont1 is Cont - 1, verify_flush(T,Naipe, Cont1); verify_flush(T,Naipe, Cont).
 
 quick_sort(List,Sorted):-q_sort(List,[],Sorted).
 q_sort([],Acc,Acc).
