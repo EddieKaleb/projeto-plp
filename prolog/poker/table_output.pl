37a38a29d49fd665527bf464837dea0dfea1d466
117110906 MARCUS VINICIUS DE FARIAS BARBOSA <vinifarias.vf@gmail.com>
2018-11-30 12:57:48 -0300

    Refatoração da tela

--- prolog/poker/table_output.pl
+++ prolog/poker/table_output.pl
@@ -9,22 +9,22 @@ printNTimes(S, N) :-
     printNTimes(S, N0).
 
 topBorder :-
-    write("/"),
-    printNTimes("-", 90),
-    write("\\\n").
+    write("┌"),
+    printNTimes("─", 90),
+    write("┐\n").
 
 bottomBorder :-
-    write("\\"),
-    printNTimes("-", 12),
-    write("T"),
-    printNTimes("-", 77),
-    write("/\n").
+    write("└"),
+    printNTimes("─", 12),
+    write("┴"),
+    printNTimes("─", 77),
+    write("┘\n").
 
 lateralBorders(N):-
     N < 1 -> true;
-    write("|"),
+    write("│"),
     spaces(90),
-    write("|\n"),
+    write("│\n"),
     N0 is N - 1,
     lateralBorders(N0).
 
@@ -41,9 +41,9 @@ lateralSpaces:-
     spaces(3).
 
 cardTop:-
-    write("/"),
-    printNTimes("-",3),
-    write("\\").
+    write("┌"),
+    printNTimes("─",3),
+    write("┐").
 
 nCardTops(N):-
     N == 0 -> true;
@@ -52,15 +52,15 @@ nCardTops(N):-
     nCardTops(N0).
 
 cardLateral:-
-    write("|"),
+    write("│"),
     spaces(3),
-    write("|").
+    write("│").
 
 cardLateralValue(Value):-
-    write("|"),
+    write("│"),
     formatInfo(Value, FormatedInfo),
     write(FormatedInfo),
-    write("|").
+    write("│").
 
 formatInfo("T", "10 "). 
 formatInfo(I, S):-
@@ -74,9 +74,9 @@ nCardLaterals(N):-
     nCardLaterals(N0).
 
 cardBottom:-
-    write("\\"),
-    printNTimes("-",3),
-    write("/").
+    write("└"),
+    printNTimes("─",3),
+    write("┘").
 
 nCardBottoms(N):-
     N == 0 -> true;
@@ -85,29 +85,29 @@ nCardBottoms(N):-
     nCardBottoms(N0).
 
 centralCards:-
-    write("|"),
+    write("│"),
     centralCardSpaces,
     nCardTops(2),
     centralCardSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     centralCardSpaces,
     nCardLaterals(2),
     centralCardSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     centralCardSpaces,
     nCardLaterals(2),
     centralCardSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     centralCardSpaces,
     nCardBottoms(2),
     centralCardSpaces,
-    write("|\n").
+    write("│\n").
 
 showProb(0, Prob):-
     get_player_pre_flop_prob(0, TurnProb),
@@ -128,39 +128,39 @@ showProb(3, Prob):-
 showProb(X, Prob):- Prob = 0.0.
 
 centralCardsWithProb:-
-    write("|"),
+    write("│"),
 
     centralCardSpaces,
     nCardTops(2),
     centralCardSpaces,
 
-    write("|\n"),
+    write("│\n"),
     
     get_player_cards(0, Card1, Card2),
     
-    write("|"),
+    write("│"),
     centralCardSpaces,
     nth0(1, Card1, Value1),
     nth0(1, Card2, Value2),
     cardLateralValue(Value1),
     cardLateralValue(Value2),
     centralCardSpaces,
-    write("|\n"),
+    write("│\n"),
 
     
 
-    write("E"),
-    printNTimes("-", 12),
-    write("\\"),
+    write("├"),
+    printNTimes("─", 12),
+    write("┐"),
     spaces(27),
     nth0(0, Card1, Naipe1),
     nth0(0, Card2, Naipe2),
     cardLateralValue(Naipe1),
     cardLateralValue(Naipe2),
     centralCardSpaces,
-    write("|\n"),
+    write("│\n"),
    
-    write("|"),
+    write("│"),
     write(" WIN: "),
 
     current_round(Turn),
@@ -171,47 +171,47 @@ centralCardsWithProb:-
     write("%"),
     numDigits(Output, Ndigits),
     spaces(5 - Ndigits),
-    write("|"),
+    write("│"),
     spaces(27),
     nCardBottoms(2),
     centralCardSpaces,
-    write("|\n").
+    write("│\n").
 
 lateralCards:-
-    write("|"),
+    write("│"),
     lateralSpaces,
     nCardTops(2),
     centralSpaces,
     nCardTops(2),
     lateralSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     lateralSpaces,
     nCardLaterals(2),
     centralSpaces,
     nCardLaterals(2),
     lateralSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     lateralSpaces,
     nCardLaterals(2),
     centralSpaces,
     nCardLaterals(2),
     lateralSpaces,
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     lateralSpaces,
     nCardBottoms(2),
     centralSpaces,
     nCardBottoms(2),
     lateralSpaces,
-    write("|\n").
+    write("│\n").
 
 flopTurnRiver:-
-    write("|"),
+    write("│"),
     spaces(30),
     nCardTops(3),
     spaces(2),
@@ -219,7 +219,7 @@ flopTurnRiver:-
     spaces(2),
     cardTop,
     spaces(31),
-    write("|\n"),
+    write("│\n"),
 
     cards_table(0, Naipe1, Value1),
     cards_table(1, Naipe2, Value2),
@@ -227,7 +227,7 @@ flopTurnRiver:-
     cards_table(3, Naipe4, Value4),
     cards_table(4, Naipe5, Value5),
 
-    write("|"),
+    write("│"),
     spaces(30),
     cardLateralValue(Value1),
     cardLateralValue(Value2),
@@ -237,9 +237,9 @@ flopTurnRiver:-
     spaces(2),
     cardLateralValue(Value5),
     spaces(31),
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     spaces(30),
     cardLateralValue(Naipe1),
     cardLateralValue(Naipe2),
@@ -249,9 +249,9 @@ flopTurnRiver:-
     spaces(2),
     cardLateralValue(Naipe5),
     spaces(31),
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     spaces(30),
     nCardBottoms(3),
     spaces(2),
@@ -259,7 +259,7 @@ flopTurnRiver:-
     spaces(2),
     cardBottom,
     spaces(31),
-    write("|\n").
+    write("│\n").
 
 role("dealer", "(D)").
 role("small", "(S)").
@@ -287,7 +287,7 @@ blinds(Player, Role):-
 
 
 centralPlayer(Player, ActualPlayer):-
-    write("|"),
+    write("│"),
     spaces(39),
     /**active*/
     PlayerId is Player - 1,
@@ -300,19 +300,19 @@ centralPlayer(Player, ActualPlayer):-
     role(PlayerRole, Role),
     write(Role),
     spaces(39),
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     spaces(40),
     write("Chips: "),
     get_player_chips(PlayerId, Chips),
     write(Chips),
     numDigits(Chips, Ndigits),
     spaces(43 - Ndigits),
-    write("|\n").
+    write("│\n").
 
 lateralPlayers(Player1, Player2, ActualPlayer):-
-    write("|"),
+    write("│"),
     spaces(2),
     /**active*/
     PlayerId1 is Player1 - 1,
@@ -340,9 +340,9 @@ lateralPlayers(Player1, Player2, ActualPlayer):-
     playing(Active2, PlayerId2, ActualPlayer),
      
     spaces(4),
-    write("|\n"),
+    write("│\n"),
 
-    write("|"),
+    write("│"),
     lateralSpaces,
     write("Chips: "),
     get_player_chips(PlayerId1, Chips1),
@@ -354,17 +354,17 @@ lateralPlayers(Player1, Player2, ActualPlayer):-
     write("Chips: "),
     write(Chips2),
     lateralSpaces,
-    write("|\n").
+    write("│\n").
 
 printPot:-
-    write("|"),
+    write("│"),
     spaces(40),
     write("Pot: "),
     pot(Pot),
     write(Pot),
     numDigits(Pot, Ndigits),
     spaces(45 - Ndigits),
-    write("|\n").
+    write("│\n").
 
 printTable:-
     actual_player(ActualPlayer),
